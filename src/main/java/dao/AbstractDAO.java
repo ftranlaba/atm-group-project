6b8fdd50bf49fa1365d742eb8ca10ae3cf8e4c65
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-08 13:48:37 -0600

    initial commit.

---
+++ src/main/java/dao/AbstractDAO.java
@@ -0,0 +1,150 @@
+package dao;
+
+import datamodels.IdInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Moussa
+ */
+public abstract class AbstractDAO<T extends IdInfo> implements IBaseDAO<T> {
+    private static final Logger LOGGER = LogManager.getLogger(AbstractDAO.class);
+    private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
+
+    @Override
+    public T getById(long id) throws SQLException {
+        String tableName = getTableName();
+        Connection connection = CONNECTION_POOL.getConnection();
+        String query = QueryUtil.entityByIdQuery(tableName);
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setLong(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (!rs.next()) {
+                    throw new SQLException("No entity found with id " + id);
+                }
+                return createEntityFromRow(rs);
+            }
+        } finally {
+            try {
+                CONNECTION_POOL.releaseConnection(connection);
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * @return gets Table Name from child class.
+     */
+    protected abstract String getTableName();
+
+    /**
+     * Creates an entity from a row in the ResultSet.
+     * Does not check if the ResultSet is empty.
+     *
+     * @param rs ResultSet from which to create the entity.
+     * @return The entity created from the first if any row of the ResultSet.
+     * @throws SQLException If a database access error occurs or this method is called on a closed result set.
+     */
+    protected abstract T createEntityFromRow(ResultSet rs) throws SQLException;
+
+    @Override
+    public void update(T entity) throws SQLException {
+        String tableName = getTableName();
+        List<String> columnNames = getColumnNames();
+        String query = QueryUtil.updateQuery(tableName, columnNames);
+        executeCommand(query, this::setUpdatePreparedStatement, entity);
+    }
+
+    /**
+     * @return gets Column Names from child class.
+     */
+    protected abstract List<String> getColumnNames();
+
+    /**
+     * @param query                   The query to execute. Can be an INSERT, UPDATE or DELETE query.
+     * @param preparedStatementSetter The method to set the PreparedStatement. Accepts a PreparedStatement and an entity.
+     * @param entity                  The entity to use to set the PreparedStatement.
+     */
+    private void executeCommand(String query, PreparedStatementSetter<T> preparedStatementSetter, T entity) throws SQLException {
+        Connection connection = CONNECTION_POOL.getConnection();
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            preparedStatementSetter.setValues(ps, entity);
+            ps.executeUpdate();
+        } finally {
+            try {
+                CONNECTION_POOL.releaseConnection(connection);
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+    }
+
+    protected void setUpdatePreparedStatement(PreparedStatement preparedStatement, T entity) throws SQLException {
+        setCreatePreparedStatement(preparedStatement, entity);
+        preparedStatement.setLong(getColumnNames().size() + 1, entity.id);
+    }
+
+    /**
+     * @param ps     PreparedStatement to set values for.
+     * @param entity Entity to set values from.
+     * @throws SQLException If a database access error occurs or this method is called on a closed result set.
+     */
+    protected abstract void setCreatePreparedStatement(PreparedStatement ps, T entity) throws SQLException;
+
+    @Override
+    public void create(T entity) throws SQLException {
+        String tableName = getTableName();
+        List<String> columnNames = getColumnNames();
+        String query = QueryUtil.createQuery(tableName, columnNames);
+        executeCommand(query, this::setCreatePreparedStatement, entity);
+    }
+
+    @Override
+    public void delete(long id) throws SQLException {
+        String tableName = getTableName();
+        Connection connection = CONNECTION_POOL.getConnection();
+        String query = QueryUtil.deleteQuery(tableName);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } finally {
+            try {
+                CONNECTION_POOL.releaseConnection(connection);
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public List<T> getAll() throws SQLException {
+        String tableName = getTableName();
+        Connection connection = CONNECTION_POOL.getConnection();
+        String query = QueryUtil.selectAllQuery(tableName);
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                List<T> entities = new ArrayList<>();
+                while (rs.next()) {
+                    T entity = createEntityFromRow(rs);
+                    entities.add(entity);
+                }
+                return entities;
+            }
+        } finally {
+            try {
+                CONNECTION_POOL.releaseConnection(connection);
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+    }
+}
