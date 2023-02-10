3ee37d00d8a30de0d30c6a3896ba9bf8631d96a7
alex-h <alexanderohernandez21@gmail.com>
2023-02-09 17:01:16 -0800

    added functionallity to user ui

--- src/main/java/terminallayer/TerminalMain.java
+++ src/main/java/terminallayer/TerminalMain.java
@@ -1,23 +1,22 @@
 package terminallayer;
 
-import datamodels.PrimaryAccount;
+import datamodels.Account;
+import datamodels.Card;
 import datamodels.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import terminallayer.exceptions.InvalidTypeException;
-import terminallayer.util.CardUtil;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import static terminallayer.util.CardUtil.*;
+import static terminallayer.util.TerminalUtil.*;
 
 
 public class TerminalMain {
+
     private static final Logger LOGGER = LogManager.getLogger("TESTLOGGER");
     private static final Scanner scan = new Scanner(System.in);
 
@@ -28,11 +27,11 @@ public class TerminalMain {
         LOGGER.info("3) Create Account");
         switch (scan.nextInt()) {
             case 1:
-                authUser();
-                userUI();
+                Account a = authUser();
+                userUI(a);
                 break;
             case 2:
-                authUser();
+               authUser();
                 adminUI();
                 break;
             case 3:
@@ -44,35 +43,34 @@ public class TerminalMain {
         scan.close();
     }
 
-    public static void userUI() throws ExecutionException, InterruptedException {
+    public static void userUI(Account a) throws ExecutionException, InterruptedException, InvalidTypeException {
         LOGGER.info("1) Check account balances");
         LOGGER.info("2) Deposit Money");
         LOGGER.info("3) Transfer Money");
-        LOGGER.info("4) Change Account Pin");
-        LOGGER.info("5) Report Card Stolen");
+        LOGGER.info("4) Report Card Stolen");
+        //finish implementation. get card and account from db
+        Card c = new Card();
         switch (scan.nextInt()) {
             case 1:
                 LOGGER.info("account balance");
+                a.getBalance();
+                LOGGER.info(a);
                 break;
             case 2:
                 LOGGER.info("How Much money would you like to deposit: ");
                 BigDecimal deposit = scan.nextBigDecimal();
                 LOGGER.info(deposit);
-                // set deposit to account
+                a.setBalance(deposit);
+                // add functionality to update account to db
                 break;
             case 3:
                 //finish transfer implementation
-                transfer();
+                makeTransfer(a);
                 break;
             case 4:
-                LOGGER.info("Change Account Pin");
-                int pin = scan.nextInt();
-                LOGGER.info(pin);
-                //add setter to change pin
-                break;
-            case 5:
                 LOGGER.info("Card Blocked");
-                //set card to blocked
+                c.setBlock(false);
+                //update db
                 break;
         }
     }
@@ -83,7 +81,7 @@ public class TerminalMain {
         LOGGER.info("3) Block Card");
         LOGGER.info("4) Unblock Card");
 
-        switch (scan.nextInt()){
+        switch (scan.nextInt()) {
             case 1:
                 LOGGER.info("Enter account id: ");
                 int id = scan.nextInt();
@@ -108,29 +106,30 @@ public class TerminalMain {
 
     public static void createAccount() throws ExecutionException, InterruptedException, InvalidTypeException {
         User u = createUser();
-        makeAccount(1);
+        LOGGER.info(u);
+        //query to get user
+        //get id from user for makeAccount(u.getId)
+        Account a = makeAccount(1);
+        //query to get id from account from user id createCard(a.getId)
+        //type from account type
+        LOGGER.info(a);
+        Card c = createCard(1, "Debit");
+        LOGGER.info(c);
     }
 
-    public static void makeAccount(int id) throws InvalidTypeException {
-        LOGGER.info("Enter Four digit pin");
-        int pin = scan.nextInt();
-        LOGGER.info(pin);
+    public static Account makeAccount(int id) throws InvalidTypeException {
         LOGGER.info("Please enter account type: ");
         LOGGER.info("1) Credit");
         LOGGER.info("2) Debit");
         LOGGER.info("3) Savings");
+
         switch (scan.nextInt()) {
             case 1:
-//                CreditCard cc = new CreditCard(cardNumGen, date(), cvcGen());
-//                Account a = new PrimaryAccount(id, pin, "Credit");
-                break;
+                return new Account(id, "Credit");
             case 2:
-//                DebitCard db = new DebitCard(cardNumGem(), date(), cvcGen());
-//                Account a = new PrimaryAccount(id, pin, "Debit");
-                break;
+                return new Account(id, "Debit");
             case 3:
-//                Account a = new PrimaryAccount(id, pin, "Savings");
-                break;
+                return new Account(id, "Savings");
             default:
                 throw new InvalidTypeException("Invalid type");
         }
@@ -138,54 +137,27 @@ public class TerminalMain {
 
     public static User createUser() throws ExecutionException, InterruptedException {
         User u = new User();
-        // add User setter methods
         CompletableFuture<Void> c = CompletableFuture.runAsync(() -> {
             LOGGER.info("Enter First Name: ");
             scan.nextLine();
             String firstName = scan.nextLine();
-            LOGGER.info(firstName);
+            u.setFirstName(firstName);
         }).thenRunAsync(() -> {
             LOGGER.info("Enter Last Name: ");
             String lastName = scan.nextLine();
-            LOGGER.info(lastName);
+            u.setLastName(lastName);
         }).thenRunAsync(() -> {
             LOGGER.info("Enter Address");
             String address = scan.nextLine();
-            LOGGER.info(address);
+            u.setAddress(address);
         }).thenRunAsync(() -> {
             LOGGER.info("Enter Phone Number: ");
             String phoneNumber = scan.nextLine();
-            LOGGER.info(phoneNumber);
+            u.setPhoneNumber(phoneNumber);
         });
         c.get();
+        //add create user to db
         return u;
     }
 
-    public static void transfer() throws ExecutionException, InterruptedException {
-        //finish implementation
-        CompletableFuture<Void> c = CompletableFuture.runAsync(() -> {
-            LOGGER.info("From: ");
-            scan.nextLine();
-            String from = scan.nextLine();
-            LOGGER.info(from);
-        }).thenRunAsync(() -> {
-            LOGGER.info("Where: ");
-            String where = scan.nextLine();
-            LOGGER.info(where);
-        }).thenRunAsync(() -> {
-            LOGGER.info("Amount: ");
-            BigDecimal amount = scan.nextBigDecimal();
-            LOGGER.info(amount);
-        });
-        c.get();
-    }
-
-    public static void authUser(){
-        LOGGER.info("Enter Account Id");
-        int id = scan.nextInt();
-        LOGGER.info(id);
-        LOGGER.info("Please Enter Pin ");
-        int pin = scan.nextInt();
-        LOGGER.info(pin);
-    }
 }
---
+++ src/main/java/terminallayer/util/TerminalUtil.java
@@ -0,0 +1,69 @@
+package terminallayer.util;
+
+import datamodels.Account;
+import datamodels.Card;
+import datamodels.Transfer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import terminallayer.exceptions.InvalidTypeException;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TerminalUtil {
+
+    private static final Logger LOGGER = LogManager.getLogger("TESTLOGGER");
+    private static final Scanner scan = new Scanner(System.in);
+
+    public final static String cardNumGen() {
+        Random rand = new Random();
+        return String.valueOf(rand.nextInt((int) Math.pow(10, 10)));
+    }
+
+    public final static int cvcGen() {
+        Random rand = new Random();
+        return rand.nextInt((int) Math.pow(4, 4));
+    }
+
+    public final static String date() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/YY");
+        return formatter.format(LocalDate.now().plusYears(5));
+
+    }
+
+    public final static Card createCard(int id, String type) {
+        return new Card(id, type, cardNumGen(), date(), cvcGen(), false);
+    }
+
+    public final static Account authUser() throws InvalidTypeException {
+        LOGGER.info("Enter Account Id");
+        int id = scan.nextInt();
+        LOGGER.info(id);
+        //get account from db
+        Account account = new Account();
+        if (account != null) {
+            return account;
+        }
+        throw new InvalidTypeException("Invalid Account id");
+
+    }
+
+    public final static Transfer makeTransfer(Account a) throws InvalidTypeException {
+        LOGGER.info("Amount For Transfer");
+        BigDecimal transferAmount = scan.nextBigDecimal();
+        BigDecimal initBalance = a.getBalance();
+        BigDecimal newBalance = initBalance.subtract(transferAmount);
+        LOGGER.info("Id for receiving account");
+        int id2 = scan.nextInt();
+        //get account by id
+        Account account = new Account();
+        BigDecimal oldBalance2 = account.getBalance();
+        BigDecimal newBalance2 = oldBalance2.add(transferAmount);
+        return  new Transfer(a.getId(), (Timestamp) new Date(), initBalance, newBalance, id2, oldBalance2, newBalance2);
+    }
+}
