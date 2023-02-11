7149042339959ef80804036a3b99fb2014c1beb3
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-11 16:03:07 -0600

    supress warning.

--- src/main/java/dao/exceptions/DAOException.java
+++ src/main/java/dao/exceptions/DAOException.java
@@ -3,6 +3,10 @@ package dao.exceptions;
 /**
  * @author Moussa
  */
+@SuppressWarnings("AlibabaClassNamingShouldBeCamel")
 public class DAOException extends Exception {
-    public DAOException(String message) {}
+
+    public DAOException(String message) {
+        super(message);
+    }
 }
