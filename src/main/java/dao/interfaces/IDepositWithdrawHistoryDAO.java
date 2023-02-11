ac5e003f6346f5c26956cacff94eb28af212d99c
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-10 18:15:19 -0600

    add logDepositOrWithdrawal method.

--- src/main/java/dao/interfaces/IDepositWithdrawHistoryDAO.java
+++ src/main/java/dao/interfaces/IDepositWithdrawHistoryDAO.java
@@ -1,9 +1,14 @@
 package dao.interfaces;
 
+import datamodels.Account;
 import datamodels.DepositWithdraw;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
 
 /**
  * @author Moussa
  */
 public interface IDepositWithdrawHistoryDAO extends IBaseDAO<DepositWithdraw> {
+    void logDepositOrWithdrawal(Account account, BigDecimal oldBalance, BigDecimal newBalance, String type) throws SQLException;
 }
