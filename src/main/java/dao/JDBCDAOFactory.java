53f4212dae7d5bfb21ec6e04eade78576aad066b
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-13 11:53:32 -0600

    initial commit.

---
+++ src/main/java/dao/JDBCDAOFactory.java
@@ -0,0 +1,44 @@
+package dao;
+
+import dao.concretedaos.*;
+import dao.interfaces.*;
+
+/**
+ * @author Moussa
+ */
+public class JDBCDAOFactory implements IDAOFactory {
+    @Override
+    public IAccountsDAO getAccountsDAO() {
+        return new AccountsDAO();
+    }
+
+    @Override
+    public IAccountAccessHistoryDAO getAccountAccessHistoryDAO() {
+        return new AccountAccessHistoryDAO();
+    }
+
+    @Override
+    public ICardsDAO getCardsDAO() {
+        return new CardsDAO();
+    }
+
+    @Override
+    public IDepositWithdrawHistoryDAO getDepositWithdrawHistoryDAO() {
+        return new DepositWithdrawHistoryDAO();
+    }
+
+    @Override
+    public ITransactionsDAO getTransactionsDAO() {
+        return new TransactionsDAO();
+    }
+
+    @Override
+    public ITransfersDAO getTransfersDAO() {
+        return new TransfersDAO();
+    }
+
+    @Override
+    public IUsersDAO getUsersDAO() {
+        return new UsersDAO();
+    }
+}
