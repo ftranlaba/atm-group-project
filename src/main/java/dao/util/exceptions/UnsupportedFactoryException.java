c58888b25f358dd4ce26b3a32e5d262e4b657985
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-14 12:01:58 -0600

    add from-cause constructor.

--- src/main/java/dao/util/exceptions/UnsupportedFactoryException.java
+++ src/main/java/dao/util/exceptions/UnsupportedFactoryException.java
@@ -4,4 +4,8 @@ public class UnsupportedFactoryException extends RuntimeException {
     public UnsupportedFactoryException(String message) {
         super(message);
     }
+
+    public UnsupportedFactoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
