7f8944f4bb53f12a40cd2869f813c82fe767be00
Moussa Mikhail <moussamikhail56@gmail.com>
2023-02-14 12:28:10 -0600

    remove throws SQLException declaration.

--- src/main/java/dao/interfaces/IDepositWithdrawHistoryDAO.java
+++ src/main/java/dao/interfaces/IDepositWithdrawHistoryDAO.java
@@ -5,12 +5,11 @@ import datamodels.Account;
 import datamodels.DepositWithdraw;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
 
 /**
  * @author Moussa
  */
 public interface IDepositWithdrawHistoryDAO extends IBaseDAO<DepositWithdraw> {
 
-    void logDepositOrWithdrawal(Account account, BigDecimal oldBalance, BigDecimal newBalance, String type) throws SQLException;
+    void logDepositOrWithdrawal(Account account, BigDecimal oldBalance, BigDecimal newBalance, String type);
 }
